538c05853bd5b2385adbd6b838dbf51e7dfd932c
grusso01 <grusso01@syr.edu>
2025-04-23 20:19:15 -0400

    second commit of the assignment

--- code/unibrow.py
+++ code/unibrow.py
@@ -8,5 +8,28 @@ import pandaslib as pl
 st.title("UniBrow")
 st.caption("The Universal data browser")
 
-# TODO Write code here to complete the unibrow.py
+uploaded = st.file_uploader("Upload a file:", type=["csv", "xlsx", "json"])
+if uploaded:
+    ext = pl.get_file_extension(uploaded.name)
+    data_frame = pl.load_file(uploaded, ext)
+    column_list = pl.get_column_names(data_frame)
+    
+    display_columns = st.multiselect("Select columns to display", column_list, default=column_list)
+    
+    if st.toggle("Filter data"):
+        col_filters = st.columns(3)
+        string_fields = pl.get_columns_of_type(data_frame, 'object')
+        filter_field = col_filters[0].selectbox("Select column to filter", string_fields)
+        
+        if filter_field:
+            options = pl.get_unique_values(data_frame, filter_field)
+            chosen_option = col_filters[1].selectbox("Select value to filter On", options)
+            output = data_frame[data_frame[filter_field] == chosen_option][display_columns]
+        else:
+            output = data_frame[display_columns]
+    else:
+        output = data_frame[display_columns]
+
+    st.dataframe(output)
+    st.dataframe(output.describe())
 
